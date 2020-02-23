e96997efb04d3c7633f664ea7bcdcd30379feff8
Paul DE TEMMERMAN <p2trman@gmail.com>
2020-02-23 15:15:11 +0100

    using 32 bits imgui draw index

--- code/imgui/src.cpp
+++ code/imgui/src.cpp
@@ -1,4 +1,11 @@
 
+//---- Using 32-bits vertex indices (default is 16-bits) is one way to allow large meshes with more than 64K vertices. 
+// Your renderer back-end will need to support it (most example renderer back-ends support both 16/32-bits indices).
+// Another way to allow large meshes while keeping 16-bits indices is to handle ImDrawCmd::VtxOffset in your renderer. 
+// Read about ImGuiBackendFlags_RendererHasVtxOffset for details.
+#define ImDrawIdx unsigned int
+
+
 #include "imgui/imgui.cpp"
 #include "imgui/imgui_widgets.cpp"
 #include "imgui/imgui_draw.cpp"
